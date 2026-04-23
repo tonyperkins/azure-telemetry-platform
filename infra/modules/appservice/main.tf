1936ac024805e4df20ea2392fb3b85de0ca3f7a5
Tony <tony@local>
2026-04-23 10:52:28 -0500

    chore: downgrade App Service Plan to F1

--- infra/modules/appservice/main.tf
+++ infra/modules/appservice/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Windows"
-  sku_name            = "B1"
+  sku_name            = "F1"
 
   tags = var.tags
 }
